856eb072eb5f1926397292a0797143eace57b9d7
Mark Cawthray <dr.cawthray@gmail.com>
2021-08-26 16:57:35 -0600

    Refactor card container component to be class

--- src/CardContainer/CardContainer.js
+++ src/CardContainer/CardContainer.js
@@ -1,24 +1,35 @@
 import './CardContainer.css';
 import Card from '../Card/Card'
+import React, { Component } from 'react';
 
-const CardContainer = ({ movies }) => {
+class CardContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedMovieId: null
+    }
+  }
 
-  const cards = movies.map(movie => {
+
+  //updateSelection method to setState on this.state.selectedMovieId to event.target.id
+  render() {
+    const cards = this.props.movies.map(movie => {
+      return (
+        <Card
+          id={movie.id}
+          key={movie.id}
+          img={movie.poster_path}
+          title={movie.title}
+          rating={movie.average_rating}    
+        />
+      )
+    })
     return (
-      <Card
-        id={movie.id}
-        key={movie.id}
-        img={movie.poster_path}
-        title={movie.title}
-        rating={movie.average_rating}    
-      />
+      <section className="CardContainer">
+        {cards}
+      </section>
     )
-  })
-  return (
-    <section className="CardContainer">
-      {cards}
-    </section>
-  )
+  }
 }
 
 export default CardContainer;
