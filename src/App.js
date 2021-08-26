2dcbd53e87ade874990736cebb29e5c2b4e83818
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-08-25 19:07:43 -0600

    Remove unnecessary import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import React, { Component } from 'react';
 import Header from './Header/Header';
 import CardContainer from './CardContainer/CardContainer';
-// import Card from './Card/Card';
 // import MovieDisplay from './MovieDisplay/MovieDisplay';
 // import MovieDetails from './MovieDetails/MovieDetails';
 import movieData from './data/dummyData';
