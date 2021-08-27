960466a499da777c2911465b02fd8289840995bd
Mark Cawthray <dr.cawthray@gmail.com>
2021-08-27 11:22:58 -0600

    Create import and export for component

---
+++ src/MovieDisplay/MovieDisplay.js
@@ -0,0 +1,12 @@
+import './MovieDisplay.css';
+// import MovieDetail from '../MovieDetail/MovieDetail';
+
+const MovieDisplay = () => {
+
+  return (
+    <p>I'm a Movie Display!</p>
+  )
+
+}
+
+export default MovieDisplay;
