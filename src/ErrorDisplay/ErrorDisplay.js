aa829b8409c62af15856a5ee986936808c9641e7
Mark Cawthray <dr.cawthray@gmail.com>
2021-08-28 14:23:15 -0600

    Add error image to error display component

--- src/ErrorDisplay/ErrorDisplay.js
+++ src/ErrorDisplay/ErrorDisplay.js
@@ -1,10 +1,15 @@
 import './ErrorDisplay.css';
+import icon from './browser.png';
 
 const ErrorDisplay = ({ errorMessage }) => {
 
+  
 
   return (
-    <h2>{`${errorMessage}: Page not found`}</h2>
+    <section className="errorDisplay">
+      <h2>{`${errorMessage}: Page not found`}</h2>
+      <img src={icon} alt='error icon'></img>
+    </section>
   )
 }
 
