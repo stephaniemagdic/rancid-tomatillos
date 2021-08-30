301a297c7731867ce442fd72ee9d314858108345
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-08-30 16:36:42 -0600

    Install cypress and setup dashboard-spec tests

---
+++ cypress/integration/dashboard_spec.js
@@ -0,0 +1,23 @@
+describe('Dashboard User Flows', () => {
+
+  describe('Feedback Loop login flows', () => {
+    it('Should confirm that true is equal to true', () => {
+      expect(true).to.equal(true)
+    });
+  });
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it( 'User should be able to click on a movie which will take them away from the page', () => {
+  //url contains "/"
+  //   //check that the card container is there
+    cy.get('main').contains('RANCID TOMATILLOS')
+    cy.get('section').get('article')
+  // cy.get('.Card').should('have.length', 2)
+  //movie is clicked  .. get element.click()
+  //check that url no longer is the url we were at
+  })
+
+})
