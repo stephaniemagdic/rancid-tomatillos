0ef01f744dc47fb1dab60986c11687ac80eba126
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-08-31 19:24:04 -0600

    Fix Jfetch syntax and component error

--- src/MovieDisplay/MovieDisplay.js
+++ src/MovieDisplay/MovieDisplay.js
@@ -12,42 +12,36 @@ class MovieDisplay extends Component {
       selectedMovie: {},
       isLoading: true,
     };
-   
   }
 
   componentDidMount = () => {
-    console.log('test!')
-    fetchData('movies/' + parseInt(this.params.id))
-      .then((data) => this.setState({selectedMovie: data.movie}))
-      .then(data => console.log(data, 'data'))
+    fetchData(`movies/${this.params.id}`)
+      .then((data) => {
+        this.setState({selectedMovie: data.movie, isLoading: false})
+      })
       .catch(err => console.log('err', err));
-    // .then(data => {
-    //   this.setState({selectedMovie: data.movie, isLoading: false})
-    // })
   }
 
 
   render() {
-
-    // const movie =(
-    //   <section className='MovieDisplay' >
-    //     <img src={this.state.selectedMovie.backdrop_path} alt={this.state.selectedMovie.title}></img>
-    //     <div 
-    //       className='backButton' 
-    //   // make this a redirect back to main
-    //     >GO BACK
-    //     </div>
-    //     <MovieDetail 
-    //      details={this.state.selectedMovie}
-    //     />
-    //   </section>
-    // )
+    const movie = (
+      <section className='MovieDisplay' >
+        <img src={this.state.selectedMovie.backdrop_path} alt={this.state.selectedMovie.title}></img>
+        <div 
+          className='backButton' 
+      // make this a redirect back to main
+        >GO BACK
+        </div>
+        <MovieDetail 
+         details={this.state.selectedMovie}
+        />
+      </section>
+    )
 
     return (
       <div >
-      hello
-        {/* {this.state.isLoading && <p>loading, please wait</p>}
-        {!this.state.isLoading && {movie}} */}
+        {this.state.isLoading && <p>loading, please wait</p>}
+        {!this.state.isLoading && movie}
       </div>
     )
   }
