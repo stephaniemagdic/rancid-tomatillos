fd14c63088223f2ec27d4cf48c5e8c8a694afc6b
Mark Cawthray <dr.cawthray@gmail.com>
2021-09-04 13:11:21 -0600

    Add link to home on header h1

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header>
-      <h1>
-        RANCID TOMATILLOS
-      </h1>
+      <Link to="/">
+        <h1>
+          RANCID TOMATILLOS
+        </h1>
+      </Link>
       <Link to="/Favorites">
         <h3>Favorites</h3>
       </Link>
