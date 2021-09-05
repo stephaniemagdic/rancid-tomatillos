ce56931242b47d3b62df4b6c00bfc795d6ddb3b6
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-05 14:14:44 -0600

    Add tests for return to home and detailDisplay flows

---
+++ cypress/integration/favorites_spec.js
@@ -0,0 +1,15 @@
+describe('Favorites User Flows', () => {
+  it('When a user clicks on a the RANCID TOMATILLOS header they should be taken back to the home page', () => {
+    cy.visit('http://localhost:3001/Favorites');
+    cy.get('h1').click()
+    cy.url().should('not.include', 'http://localhost:3001/Favorites')
+  })
+
+  it('User should be able to click on a movie which will take them away from the page', () => {
+    cy.visit('http://localhost:3001/movies/694919')
+    cy.get('button').first().click()
+    cy.visit('http://localhost:3001/Favorites');
+    cy.get('article').eq(0).click().url().should('include', '/694919')
+  })
+
+})
