c0abfdb48bfd02e5120a8c206c1fd3ae6426ef57
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-05 16:54:52 -0600

    Add test for undefined url

--- cypress/integration/errorDisplay_spec.js
+++ cypress/integration/errorDisplay_spec.js
@@ -3,4 +3,9 @@ describe('Error Display User Flows', () => {
     cy.visit('http://localhost:3001/undefined')
     .get('button').click().get('h1').contains('RANCID TOMATILLOS')
   })
+  
+  it('If user alters a url path to an undefined path, it should bring them to an error display page', () => {
+    cy.visit('http://localhost:3001/hotdogs')
+    .get('h2').contains('Page Not Found')
+  })
 })
