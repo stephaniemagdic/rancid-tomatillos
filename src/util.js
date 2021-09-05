4c76d4b34f8b6870170973535d2f4d7f309be6a4
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-05 17:07:20 -0600

    Remove code comments

--- src/util.js
+++ src/util.js
@@ -3,11 +3,8 @@
 export const fetchData = (url) => {
   return fetch(url)
   .then((response) => checkForErrors(response))
-    .then(data => {
-      console.log("fetch data in fetch all data", data, "for this url", url)
-      return data
-    })
-  }
+    .then(data => data)
+}
 
 export const postData = (dataObject) => {
   return fetch(`http://localhost:3002/api/v1/favoritesList`, {
@@ -19,20 +16,14 @@ export const postData = (dataObject) => {
   })
 }
 
-
 export const deleteData = (id) => {
-  console.log('In fetch for delete data --->', id);
   return fetch('http://localhost:3002/api/v1/favoritesList', {
     method: 'DELETE',
-    //commenting out the body fixes it the unfound fetch.
- 
     body: JSON.stringify({id}),
-
     headers: {
       'Content-type': 'application/json',
-      // 'Access-Control-Allow-Origin': *
     }
-  }).then((data) => console.log("deleteData", data))
+  })
   .catch((err) => console.log("id error", err))
 }
 
