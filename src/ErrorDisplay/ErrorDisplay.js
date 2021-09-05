914d86720c0f313eed7dffe721641d19387a1d8e
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-05 13:07:49 -0600

    Add conditional rendering based on error message

--- src/ErrorDisplay/ErrorDisplay.js
+++ src/ErrorDisplay/ErrorDisplay.js
@@ -1,14 +1,25 @@
 import './ErrorDisplay.css';
 import icon from '../Assets/browser.png';
 import Header from '../Header/Header';
+import { Link } from 'react-router-dom';
 
 const ErrorDisplay = ({ errorMessage }) => {
 
+  let displayText = '';
+  let button = null;
+
+  if (errorMessage) {
+    displayText = 'Server Error'
+  } else if (!errorMessage) {
+    displayText = 'Page Not Found'
+    button = <Link to="/"><button>TAKE ME TO RANCID TOMATILLOS HOMEPAGE</button></Link>
+  }
 
   return (
     <main>
       <section className="errorDisplay">
-        <h2>{`${errorMessage}: Page not found`}</h2>
+        <h2>{displayText}</h2>
+        {button && button}
         <img src={icon} alt='error icon'></img>
       </section>
     </main>
