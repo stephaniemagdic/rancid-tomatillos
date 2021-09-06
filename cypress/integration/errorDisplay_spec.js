9bd4353229ba9477f9b4e734e1070f5d380e4df2
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-06 15:42:54 -0600

    Change local host port

--- cypress/integration/errorDisplay_spec.js
+++ cypress/integration/errorDisplay_spec.js
@@ -1,11 +1,11 @@
 describe('Error Display User Flows', () => {
   it('If user typed in an invalid URL, the user should be able to click on a button to be taken to the RANCID TOMATILLOS homepage', () => {
-    cy.visit('http://localhost:3001/undefined')
+    cy.visit('http://localhost:3000/undefined')
     .get('button').click().get('h1').contains('RANCID TOMATILLOS')
   })
   
   it('If user alters a url path to an undefined path, it should bring them to an error display page', () => {
-    cy.visit('http://localhost:3001/hotdogs')
+    cy.visit('http://localhost:3000/hotdogs')
     .get('h2').contains('Page Not Found')
   })
 })
