98bd835536a3be50f6560866d8409445b8303308
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-06 08:16:50 -0600

    Add react player for movie banner

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,6 +1,9 @@
 import './Header.css';
 import { Link } from 'react-router-dom';
 
+import React from 'react'
+import ReactPlayer from 'react-player'
+
 const Header = () => {
   return (
     <header>
@@ -9,6 +12,28 @@ const Header = () => {
           RANCID TOMATILLOS
         </h1>
       </Link>
+      {/* <div className="banner">
+          <ReactPlayer 
+              url='https://www.youtube.com/watch?v=cDTg62vsV4U'
+              className="react-player"
+              width="100%"
+              height="100%"
+              controls={false}
+              muted={true}
+              playing={true}
+              loop={true}
+              config={{
+                youtube: {
+                  playerVars: {
+                    disablekb: 1,
+                    fs: 0,
+                    iv_load_policy: 3,
+                    modestbranding: 1,
+                  },
+                },
+              }}
+          />  
+        </div> */}
       <Link to="/Favorites">
         <h3>Favorites</h3>
       </Link>
