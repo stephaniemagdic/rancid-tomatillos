a2ae24363e389f978c041bbcb0a25d68875042ce
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-06 19:40:44 -0600

    Add hasMovieBanner prop to components with rendering card container

--- src/App/App.js
+++ src/App/App.js
@@ -42,6 +42,7 @@ class App extends Component {
           <Route exact path="/" render={ () => {
             return <CardContainer 
               movies={this.state.movies}
+              hasMovieBanner={true}
             />
           }}
           />
